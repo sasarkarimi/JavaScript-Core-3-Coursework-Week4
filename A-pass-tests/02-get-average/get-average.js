5f30f074d03a2a64199b9640689860e5a24a9b1f
sasarkarimi <sahar.karimi90@outlook.com>
2022-11-22 20:06:05 +0000

    Complete A-pass-tests folder

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,10 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
-
-module.exports = average;
+function average(numbers) {
+    let getnum = numbers.filter((num) => typeof num === "number");
+    let average = getnum.reduce((a, b) => a + b, 0) / getnum.length;
+    return average;
+ }
+ 
+ module.exports = average;
